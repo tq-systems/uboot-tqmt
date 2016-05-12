tq-systems/uboot-tqmt

commit 222a655dd50c826e3d8d801bb8e58289be75df7c

add RCW serdes protocol configuration "0x8E"

arch/powerpc/cpu/mpc85xx/t1040_serdes.c
@@ -39,6 +39,8 @@ static u8 serdes_cfg_tbl[][SRDS_MAX_LANES] = {
 		PCIE2, PCIE3, SGMII_SW1_MAC4, SATA1},
 	[0x8D] = {PCIE1, SGMII_SW1_MAC3, SGMII_SW1_MAC1, SGMII_SW1_MAC2,
 		PCIE2, SGMII_SW1_MAC6, SGMII_SW1_MAC4, SGMII_SW1_MAC5},
+	[0x8E] = {PCIE1, SGMII_FM1_DTSEC3, SGMII_FM1_DTSEC1, SGMII_FM1_DTSEC2,
+		AURORA, PCIE2, SGMII_FM1_DTSEC4, SATA1},
 	[0x8F] = {PCIE1, SGMII_FM1_DTSEC3, SGMII_FM1_DTSEC1, SGMII_FM1_DTSEC2,
 		AURORA, NONE, SGMII_FM1_DTSEC4, SGMII_FM1_DTSEC5},
 	[0xA5] = {PCIE1, SGMII_FM1_DTSEC3, SGMII_FM1_DTSEC1, SGMII_FM1_DTSEC2,

commit 5ecda3937b9063bc50719669906b9a6a990778cf

corrected typo introduced in commit 222a655dd50c826e3d8d801bb8e58289be75df7c RCW serdes protocol configuration

arch/powerpc/cpu/mpc85xx/t1040_serdes.c
@@ -40,7 +40,7 @@ static u8 serdes_cfg_tbl[][SRDS_MAX_LANES] = {
 	[0x8D] = {PCIE1, SGMII_SW1_MAC3, SGMII_SW1_MAC1, SGMII_SW1_MAC2,
 		PCIE2, SGMII_SW1_MAC6, SGMII_SW1_MAC4, SGMII_SW1_MAC5},
 	[0x8E] = {PCIE1, SGMII_FM1_DTSEC3, SGMII_FM1_DTSEC1, SGMII_FM1_DTSEC2,
-		AURORA, PCIE2, SGMII_FM1_DTSEC4, SATA1},
+		AURORA, PCIE3, SGMII_FM1_DTSEC4, SATA1},
 	[0x8F] = {PCIE1, SGMII_FM1_DTSEC3, SGMII_FM1_DTSEC1, SGMII_FM1_DTSEC2,
 		AURORA, NONE, SGMII_FM1_DTSEC4, SGMII_FM1_DTSEC5},
 	[0xA5] = {PCIE1, SGMII_FM1_DTSEC3, SGMII_FM1_DTSEC1, SGMII_FM1_DTSEC2,