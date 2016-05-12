4e40c283e5f3ccb7771ba026f748d6951d6c8dcf
Stefan Lange <s.lange@gateware.de>
2016-05-12 09:33:46 +0200

    add device-specific initialisation routines for SGMII ethernet phys located on the STKT104x

--- board/tqc/tqmt1042/eth.c
+++ board/tqc/tqmt1042/eth.c
@@ -20,20 +20,19 @@
  */
 int board_phy_config(struct phy_device *phydev)
 {
-
-#define STKT104X_EC1_PHYAD            0x0E
-#define STKT104X_EC2_PHYAD            0x05
 #define MII_DP83867_REGCR             0x000D
 #define MII_DP83867_ADDAR             0x000E
 #define MII_DP83867_REGCR_AD          0x001F
 #define MII_DP83867_REGCR_DAT         0x401F
 #define MII_DP83867_RGMIICTL          0x0032
 #define MII_DP83867_RGMIIDCTL         0x0086
+#define MII_DP83867_BYTEORDER         0x00DC
+#define MII_88E1340_PAGEAD            0x0016
 
 	if (phydev->drv->config)
 		phydev->drv->config(phydev);
 
-	if (phydev->addr == STKT104X_EC1_PHYAD) {
+	if (phydev->addr == CONFIG_SYS_RGMII1_PHY_ADDR) {
 		/* set EC1 RGMII delay (extended write) */
 		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_REGCR,MII_DP83867_REGCR_AD);
 		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_ADDAR,MII_DP83867_RGMIIDCTL);
@@ -47,7 +46,7 @@ int board_phy_config(struct phy_device *phydev)
 		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_ADDAR,0x00D3);
 	}
 
-	if (phydev->addr == STKT104X_EC2_PHYAD) {
+	if (phydev->addr == CONFIG_SYS_RGMII2_PHY_ADDR) {
 		/* set EC2 RGMII delay (extended write) */
 		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_REGCR,MII_DP83867_REGCR_AD);
 		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_ADDAR,MII_DP83867_RGMIIDCTL);
@@ -59,6 +58,39 @@ int board_phy_config(struct phy_device *phydev)
 		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_ADDAR,MII_DP83867_RGMIICTL);
 		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_REGCR,MII_DP83867_REGCR_DAT);
 		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_ADDAR,0x00D3);
+	}
+
+	if (phydev->addr == CONFIG_SYS_SGMII2_PHY_ADDR) {
+		/* set DP83867 byteordering hidden register (extended write) */
+		/* workaround for pre-production grade silicon */
+		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_REGCR,MII_DP83867_REGCR_AD);
+		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_ADDAR,MII_DP83867_BYTEORDER);
+		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_REGCR,MII_DP83867_REGCR_DAT);
+		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_ADDAR,0x3800);
+	}
+
+	if (phydev->addr == CONFIG_SYS_SGMII4_PHY_ADDR) {
+		/* set DP83867 byteordering hidden register (extended write) */
+		/* workaround for pre-production grade silicon */
+		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_REGCR,MII_DP83867_REGCR_AD);
+		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_ADDAR,MII_DP83867_BYTEORDER);
+		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_REGCR,MII_DP83867_REGCR_DAT);
+		phy_write(phydev,MDIO_DEVAD_NONE,MII_DP83867_ADDAR,0x3800);
+	}
+
+	if (phydev->addr == CONFIG_SYS_SGMII3_PHY_ADDR) {
+		/* do 88E1340 phy initialisation #1 */
+		/* as described in MV-S301615 release note */
+		phy_write(phydev,MDIO_DEVAD_NONE,MII_88E1340_PAGEAD,0xFF);
+		phy_write(phydev,MDIO_DEVAD_NONE,0x18,0x2800);
+		phy_write(phydev,MDIO_DEVAD_NONE,0x17,0x2001);
+		phy_write(phydev,MDIO_DEVAD_NONE,MII_88E1340_PAGEAD,0x00);
+		/* do 88E1340 phy initialisation #2 */
+		/* as described in MV-S301615 release note */
+		phy_write(phydev,MDIO_DEVAD_NONE,MII_88E1340_PAGEAD,0x00);
+		phy_write(phydev,MDIO_DEVAD_NONE,0x1d,0x0003);
+		phy_write(phydev,MDIO_DEVAD_NONE,0x1e,0x0002);
+		phy_write(phydev,MDIO_DEVAD_NONE,0x1d,0x0000);
 	}
 
 	return 0;
