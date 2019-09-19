598f6c2796964eab8a3be163d60da7d9257ad3a2
Max Merchel <Max.Merchel@tq-group.com>
2019-09-19 15:18:29 +0200

    tqmt1022: add header
    Signed-off-by: Max Merchel <Max.Merchel@tq-group.com>

---
+++ include/configs/TQMT1022.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2015 TQ Systems GmbH
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef __CONFIG_TQMT1022_H
+#define __CONFIG_TQMT1022_H
+
+/* name and path of kernel image and device tree
+ * set in default environment
+ * for nfs boot and sd card boot
+ */
+#define CONFIG_BOOTFILE	"tqmt1022-linuximage"
+#define FDTFILE		"tqmt1022-stkt104x.dtb"
+
+#include <configs/TQMT104x.h>
+
+#endif	/* __CONFIG_TQMT1022_H */
