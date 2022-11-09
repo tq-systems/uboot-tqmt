05cc976634927205bbc4598ac9d2fdb92011b2c7
Gregor Herburger <gregor.herburger@tq-group.com>
2022-11-09 11:49:30 +0100

    board: tqc: tqmt104x: update ddr timings
    Update DDR-Timing as suggested by HW colleague.
    
    Signed-off-by: Gregor Herburger <gregor.herburger@tq-group.com>

--- board/tqc/tqmt104x/ddr.h
+++ board/tqc/tqmt104x/ddr.h
@@ -8,23 +8,23 @@
 #define __DDR_H__
 
 
-#define DDR_SDRAM_CFG                   0x67040008
+#define DDR_SDRAM_CFG                   0xE7200000
 #define DDR_CS0_BNDS                    0x0000007F
 #define DDR_CS0_CONFIG                  0x80044302
 #define DDR_CS0_CONFIG2                 0x00000000
-#define DDR_TIMING_CFG_0                0x0064400C
-#define DDR_TIMING_CFG_1                0xBCB48C56
-#define DDR_TIMING_CFG_2                0x0040C120
+#define DDR_TIMING_CFG_0                0x8055000C
+#define DDR_TIMING_CFG_1                0xBCB48E56
+#define DDR_TIMING_CFG_2                0x0041111C
 #define DDR_TIMING_CFG_3                0x010C1000
-#define DDR_TIMING_CFG_4                0x00000001
+#define DDR_TIMING_CFG_4                0x00220001
 #define DDR_TIMING_CFG_5                0x04401400
-#define DDR_SDRAM_CFG_2                 0x00401010
-#define DDR_SDRAM_MODE                  0x00441C70
+#define DDR_SDRAM_CFG_2                 0x00401050
+#define DDR_SDRAM_MODE                  0x00441C71
 #define DDR_SDRAM_MODE_2                0x00180000
-#define DDR_SDRAM_INTERVAL              0x0C300100
-#define DDR_DDR_WRLVL_CNTL              0x8645C607
-#define DDR_DDR_WRLVL_CNTL_2            0x09050600
-#define DDR_DDR_WRLVL_CNTL_3            0x06050600
+#define DDR_SDRAM_INTERVAL              0x0C300618
+#define DDR_DDR_WRLVL_CNTL              0x8655F608
+#define DDR_DDR_WRLVL_CNTL_2            0x09080807
+#define DDR_DDR_WRLVL_CNTL_3            0x0706060A
 #define DDR_DDR_CDR1                    0x80040000
 #define DDR_DDR_CDR2                    0x00000001
 #define DDR_SDRAM_CLK_CNTL              0x02000000
